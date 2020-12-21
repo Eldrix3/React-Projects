4af5ae41ddaddada6b7bb04299720c4f2051ddbc
Eldrix3 <addi.geilke@gmail.com>
2020-12-21 17:42:39 +0100

    new Commit

--- redditapibeta/src/ReddditApi.js
+++ redditapibeta/src/ReddditApi.js
@@ -1,7 +1,7 @@
 
 export default {
     search: function(searchTerm) {
-      return fetch("http://www.reddit.com/search.json?q="+searchTerm+'&raw_json=1'+'&sort=new'+'&limit=100'+'&include_over_18=on')
+      return fetch("http://www.reddit.com/search.json?q="+searchTerm+'&raw_json=1'+'&sort=new'+'&limit=200'+'&include_over_18=on')
         .then(res => res.json())
         .then(data => data.data.children.map(data => data.data))
         .catch(err => console.log("ERROR"));
